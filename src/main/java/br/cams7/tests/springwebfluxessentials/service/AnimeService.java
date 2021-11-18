df11de23ec5c99a865991398fd23d8df49c5beb9
César Magalhães <cesar.magalhaes-ext@ab-inbev.com>
2021-11-18 12:53:36 -0300

    Return Not Found exeption when anime isn't found by id

--- src/main/java/br/cams7/tests/springwebfluxessentials/service/AnimeService.java
+++ src/main/java/br/cams7/tests/springwebfluxessentials/service/AnimeService.java
@@ -3,7 +3,9 @@ package br.cams7.tests.springwebfluxessentials.service;
 import br.cams7.tests.springwebfluxessentials.domain.Anime;
 import br.cams7.tests.springwebfluxessentials.repository.AnimeRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -13,10 +15,14 @@ public class AnimeService {
   private final AnimeRepository repository;
 
   public Flux<Anime> findAll() {
-    return repository.findAll();
+    return repository.findAll(); // .log();
   }
 
   public Mono<Anime> findById(Long id) {
-    return repository.findById(id);
+    return repository.findById(id).switchIfEmpty(monoResponseStatusNotFoundException()); // .log();
+  }
+
+  private static <T> Mono<T> monoResponseStatusNotFoundException() {
+    return Mono.error(new ResponseStatusException(HttpStatus.NOT_FOUND));
   }
 }
