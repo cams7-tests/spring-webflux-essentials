c6563a5bbba13d2d95ada913237d2e9aa00ca438
César Magalhães <cesar.magalhaes-ext@ab-inbev.com>
2021-11-27 11:51:39 -0300

    Add logging methods with AspectJ

--- src/main/java/br/cams7/tests/springwebfluxessentials/service/AnimeService.java
+++ src/main/java/br/cams7/tests/springwebfluxessentials/service/AnimeService.java
@@ -1,8 +1,10 @@
 package br.cams7.tests.springwebfluxessentials.service;
 
 import static br.cams7.tests.springwebfluxessentials.utils.CommonExceptions.responseNotFoundException;
+import static java.time.temporal.ChronoUnit.MILLIS;
 
 import br.cams7.tests.springwebfluxessentials.domain.Anime;
+import br.cams7.tests.springwebfluxessentials.logging.LogEntryExit;
 import br.cams7.tests.springwebfluxessentials.repository.AnimeRepository;
 import java.util.Set;
 import lombok.RequiredArgsConstructor;
@@ -16,23 +18,28 @@ import reactor.core.publisher.Mono;
 public class AnimeService {
   private final AnimeRepository repository;
 
+  @LogEntryExit(unit = MILLIS)
   public Flux<Anime> findAll() {
     return repository.findAll();
   }
 
+  @LogEntryExit(showArgs = true, unit = MILLIS)
   public Mono<Anime> findById(Long id) {
     return repository.findById(id).switchIfEmpty(responseNotFoundException());
   }
 
+  @LogEntryExit(showArgs = true, unit = MILLIS)
   public Mono<Anime> save(Anime anime) {
     return repository.save(anime);
   }
 
+  @LogEntryExit(showArgs = true, unit = MILLIS)
   @Transactional
   public Flux<Anime> saveAll(Set<Anime> animes) {
     return repository.saveAll(animes);
   }
 
+  @LogEntryExit(showArgs = true, unit = MILLIS)
   public Mono<Void> update(Anime anime) {
     return findById(anime.getId())
         .map(animeFound -> anime.withId(animeFound.getId()))
@@ -40,6 +47,7 @@ public class AnimeService {
         .then();
   }
 
+  @LogEntryExit(showArgs = true, unit = MILLIS)
   public Mono<Void> delete(Long id) {
     return findById(id).flatMap(repository::delete);
   }
